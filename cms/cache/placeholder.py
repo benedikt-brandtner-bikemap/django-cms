236da9fadcc3971dc39e9087844b2d08053b4e3f
Benedikt Brandtner <benedikt@bikemap.net>
2018-02-03 14:52:34 +0100

    removed NO_CACHE_COOKIE logic and replaced it with LOGGED_IN_COOKIE logic

--- cms/cache/placeholder.py
+++ cms/cache/placeholder.py
@@ -6,12 +6,13 @@ from cms.cache import _get_cache_version, _set_cache_version, _clean_key, _get_c
 from cms.utils import get_cms_setting
 
 
-def _placeholder_cache_key(placeholder, lang, unit, super_user):
-    if super_user is not False:
-        super_user = 'admin'
+def _placeholder_cache_key(placeholder, lang, unit, logged_in):
+    if logged_in:
+        logged_in = 'logged_in'
     else:
-        super_user = 'user'
-    cache_key = '%srender_placeholder:%s.%s.%s.%s' % (get_cms_setting("CACHE_PREFIX"), placeholder.pk, str(lang), str(unit), super_user)
+        logged_in = 'anonymous'
+
+    cache_key = '%srender_placeholder:%s.%s.%s.%s' % (get_cms_setting("CACHE_PREFIX"), placeholder.pk, str(lang), str(unit), logged_in)
     if settings.USE_TZ:
         tz_name = force_text(get_current_timezone_name(), errors='ignore')
         cache_key += '.%s' % tz_name.encode('ascii', 'ignore').decode('ascii').replace(' ', '_')
@@ -24,7 +25,7 @@ def set_placeholder_cache(placeholder, request, lang, content):
     """
     from django.core.cache import cache
     unit = request.COOKIES.get(settings.UNIT_COOKIE_NAME, 'metric')
-    super_user = request.COOKIES.get(settings.NO_CACHE_COOKIE_NAME, False)
+    super_user = request.COOKIES.get(settings.LOGGED_IN_CACHE_COOKIE_NAME, False)
     cache.set(_placeholder_cache_key(placeholder, lang, unit, super_user),
               content,
               get_cms_setting('CACHE_DURATIONS')['content'],
@@ -38,7 +39,7 @@ def get_placeholder_cache(placeholder, request, lang):
     """
     from django.core.cache import cache
     unit = request.COOKIES.get(settings.UNIT_COOKIE_NAME, 'metric')
-    super_user = request.COOKIES.get(settings.NO_CACHE_COOKIE_NAME, False)
+    super_user = request.COOKIES.get(settings.LOGGED_IN_CACHE_COOKIE_NAME, False)
     return cache.get(_placeholder_cache_key(placeholder, lang, unit, super_user),
                      version=_get_cache_version())
 
@@ -50,20 +51,20 @@ def clear_placeholder_cache(placeholder, lang):
         cache.delete(_placeholder_cache_key(placeholder, lang, unit, False), version=_get_cache_version())
 
 
-def _placeholder_page_cache_key(page_lookup, lang, unit, site_id, placeholder_name, super_user):
-    if super_user is not False:
-        super_user = 'admin'
+def _placeholder_page_cache_key(page_lookup, lang, unit, site_id, placeholder_name, logged_in):
+    if logged_in:
+        logged_in = 'logged_in'
     else:
-        super_user = 'user'
+        logged_in = 'anonymous'
     base_key = _get_cache_key('_show_placeholder_for_page', page_lookup, lang, site_id)
-    base_key += '_unit:{0}_type:{1}'.format(str(unit), super_user)
+    base_key += '_unit:{0}_type:{1}'.format(str(unit), logged_in)
     return _clean_key('%s_placeholder:%s' % (base_key, placeholder_name))
 
 
 def get_placeholder_page_cache(page_lookup, request, lang, site_id, placeholder_name):
     from django.core.cache import cache
     unit = request.COOKIES.get(settings.UNIT_COOKIE_NAME, 'metric')
-    super_user = request.COOKIES.get(settings.NO_CACHE_COOKIE_NAME, False)
+    super_user = request.COOKIES.get(settings.LOGGED_IN_CACHE_COOKIE_NAME, False)
     return cache.get(_placeholder_page_cache_key(page_lookup, lang, unit, site_id, placeholder_name, super_user),
                      version=_get_cache_version())
 
@@ -71,7 +72,7 @@ def get_placeholder_page_cache(page_lookup, request, lang, site_id, placeholder_
 def set_placeholder_page_cache(page_lookup, request, lang, site_id, placeholder_name, content):
     from django.core.cache import cache
     unit = request.COOKIES.get(settings.UNIT_COOKIE_NAME, 'metric')
-    super_user = request.COOKIES.get(settings.NO_CACHE_COOKIE_NAME, False)
+    super_user = request.COOKIES.get(settings.LOGGED_IN_CACHE_COOKIE_NAME, False)
     cache.set(_placeholder_page_cache_key(page_lookup, lang, unit, site_id, placeholder_name, super_user),
               content,
               get_cms_setting('CACHE_DURATIONS')['content'], version=_get_cache_version())
